1c8bf1cff2bf7484fa7048a08987dbda7e47b7a7
legend270 <165806578+legend270@users.noreply.github.com>
2025-06-13 18:30:51 +0000

    Add files via upload

---
+++ DAY1.py
@@ -0,0 +1,8 @@
+#Print a greeting message
+print("Hello World")
+
+#perform basic arithmetic and print the results
+print("2 + 2 =", 2 + 2)
+print("5 * 34 =", 5 * 34)
+print("5840 - 40 =", 5840 - 40)
+print("2000 / 50 =", 2000 / 50)

aa6ac8b7ae3185f9476549eef7ab0ffad28aceeb
legend270 <165806578+legend270@users.noreply.github.com>
2025-06-14 09:23:36 +0000

    Add files via upload

---
+++ DAY2.py
@@ -0,0 +1,12 @@
+#Day 2 of the 90 days python challenge
+
+#input user name
+name = input("Please Enter your name: ")
+#input user age
+age = input("What is your age? ")
+#calculate year of birth 
+YOB = 2025-int(age)
+#print outputs
+print(f"Hello, {name}")
+print(f"you are {age} years old")
+print(f"your year of birth is {YOB}")e

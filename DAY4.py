c1d30c2af4dafd5b8928d4aff1ad662831fdcf7a
legend270 <165806578+legend270@users.noreply.github.com>
2025-06-15 13:54:31 +0000

    Add files via upload

---
+++ DAY4.py
@@ -0,0 +1,10 @@
+#Day 4 of the 90 days challenge
+
+# Countdown from 10 to 1
+count = 10
+
+print("Countdown starting...")
+while count > 0:
+    print(count)
+    count -= 1  # same as count = count - 1
+print("Blast off!")
